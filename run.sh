b6fe7d7a77980c940509c5ceb7728e4d233359c1
wataru0 <wataru.tv.nanao@gmail.com>
2021-10-18 01:56:29 +0900

    add: baseline-v2

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@
 # 実行コマンド
 # bash run.sh --train ...
 
-python run.py --train --training_id=baseline --seed=1
-python run.py --train --training_id=baseline --seed=2
-python run.py --train --training_id=baseline --seed=3
-python run.py --train --training_id=baseline --seed=4
-python run.py --train --training_id=baseline --seed=5
+# python run.py --train --training_id=baseline --seed=1
+# python run.py --train --training_id=baseline --seed=2
+# python run.py --train --training_id=baseline --seed=3
+# python run.py --train --training_id=baseline --seed=4
+# python run.py --train --training_id=baseline --seed=5
+python run.py --train --training_id=baseline-v2 --seed=1
+python run.py --train --training_id=baseline-v2 --seed=2
+python run.py --train --training_id=baseline-v2 --seed=3
+python run.py --train --training_id=baseline-v2 --seed=4
+python run.py --train --training_id=baseline-v2 --seed=5
