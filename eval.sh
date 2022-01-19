98dad75866d945f33230bf199dd5dd08a8210852
wataru0 <wataru.tv.nanao@gmail.com>
2022-01-19 18:14:30 +0900

    [new]

---
+++ eval.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# LD_PRELOAD= python eval.py --agent_id=baseline-v2
+# LD_PRELOAD= python eval.py --agent_id=udr
+# LD_PRELOAD= python eval.py --agent_id=cdr-v1
+# LD_PRELOAD= python eval.py --agent_id=cdr-v1_
+# LD_PRELOAD= python eval.py --agent_id=cdr-v2
+# LD_PRELOAD= python eval.py --agent_id=cdr-v2_
+# LD_PRELOAD= python eval.py --agent_id=lcdr-v1
+# LD_PRELOAD= python eval.py --agent_id=lcdr-v2
+# # LD_PRELOAD= python eval.py --agent_id=acdr_prot
+# # LD_PRELOAD= python eval.py --agent_id=acdr_prot_softmax_t=1
+# LD_PRELOAD= python eval.py --agent_id=acdr_prot_bayes
+# LD_PRELOAD= python eval.py --agent_id=acdr_prot_bayes_4e6
+# # LD_PRELOAD= python eval.py --agent_id=baseline
+
+# LD_PRELOAD= python eval.py --agent_id=baseline_4e6
+# LD_PRELOAD= python eval.py --agent_id=cdr-v1_16e6
+# LD_PRELOAD= python eval.py --agent_id=cdr-v2_16e6
+
+# LD_PRELOAD= python eval.py --agent_id=cdr-v1_bf50
+# LD_PRELOAD= python eval.py --agent_id=cdr-v2_bf50
+
+LD_PRELOAD= python eval.py --agent_id=lcdr-v2_4e6
+LD_PRELOAD= python eval.py --agent_id=cdr-v1_bf10
+LD_PRELOAD= python eval.py --agent_id=cdr-v2_bf10
